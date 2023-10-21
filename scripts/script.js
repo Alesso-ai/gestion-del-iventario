aea376771fb91bdeee41f2e1b2b54def1cba16a6
alex <ruizalexruiz24@hotmail.es>
2023-10-21 12:31:13 +0200

    Actualizando los JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,21 @@
 //Importar un JS 
 /*Alejandro Ruiz Prieto:
-    Git hub:
-    Listar Prodcutos
+    Git hub: https://github.com/Alesso-ai/gestion-del-iventario.git
+    Listar Productos
 */ 
+
+/*Importaciones con otros JS*/ 
 import{ listProducts } from "./listProducts.js"
+import { inventory } from "./product.js"
 
 document.addEventListener("DOMContentLoaded", listProducts);
 
 const searchProduct = () => {
+    const productSearched = document.getElementById("busqueda").value;
 
+    array.forEach(element => {
+        
+    });
 
 
     
